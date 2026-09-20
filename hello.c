2c9aaf1f3160369322ac89d9ccd12ab247fd0c16
Jon Jiao <jonjiao13@gmail.com>
2026-09-19 19:39:48 -0500

    Print name and email 20 times

--- hello.c
+++ hello.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Jon\n");
-  printf("jonjiao13@gmail.com\n");
+  int i = 0;
+
+  for (int i = 0; i < 20; i++) {
+    printf("Jon\n");	
+    printf("jonjiao13@gmail.com\n");
+  }
 
   return 0;
 }
